d7733c822d5275f64ae9f8a78fc221816608c28f
Kankana Kashyap <kashyap.kankana@gmail.com>
2021-08-01 20:15:34 +0530

    video 13 commit

---
+++ parent to child/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{title}}</h1>
+    <Child v-on:changeTitle="updateTitle($event)" />
+  </div>
+</template>
+
+<script>
+import Child from './components/Child.vue'
+export default {
+  name: 'App',
+  components: {
+    Child
+  },
+  data(){
+    return {title:"props tutorial"}
+  },
+  methods:{
+    updateTitle(title)
+    {
+      this.title=title
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ parent to child/Child.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+        <!-- <h1>Send props from child to parent</h1> -->
+        <button v-on:click="updateTitle" >Update parent title</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Child',
+    methods:{
+        updateTitle(){
+            console.log("Hello from inside method")
+            this.$emit('changeTitle',"parent updated")
+    }
+    }
+}
+</script>
