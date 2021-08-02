d167d0de16d97640ea9e9ea1154a5a3abcd8701e
Kankana Kashyap <kashyap.kankana@gmail.com>
2021-08-02 08:42:38 +0530

    lifecycle methods

---
+++ lifecycle methods/beforeCreate and created/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{dataValue}}</h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'Home',
+    data()
+    {
+        return{
+            dataValue:"Data is set here inside the component.\n"
+        }
+    },
+    beforeCreate()
+    {
+        this.dataValue="Data is set here in beforeCreate().\n";
+        console.warn("Called 1st\n",this.dataValue)
+    },
+    created()
+    {
+        this.dataValue="Again data is set here in created()"
+        console.warn("Called 2nd\n",this.dataValue);
+    }
+}
+</script>
