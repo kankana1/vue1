4eee20a97ee2369eeb8bd1eab2b7809bda6060a5
Kankana Kashyap <kashyap.kankana@gmail.com>
2021-08-02 16:35:19 +0530

    Add files via upload

---
+++ submit data(post call) in api/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>Submit form Data to Api(post call) in vue js</h1>
+    <PostComponent />
+  </div>
+</template>
+
+<script>
+import PostComponent from './components/PostComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    PostComponent
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ submit data(post call) in api/components/PostComponent.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+        <form @submit="submitData" method="post">
+            <input type="text" name="title" placeholder="Enter the title" v-model="post.title" /><br><br>
+            <input type="text" name="author" placeholder="Enter the author" v-model="post.author" /><br><br>
+            <button type="submit">Post</button>
+        </form>
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+Vue.use(VueAxios,axios)
+export default {
+    name:'PostComponent',
+    data()
+    {
+        return{
+            post:{
+                title:null,
+                author:null
+            }
+        }
+    }, 
+    methods:
+    {
+        submitData(e)
+        {
+            this.axios.post("http://localhost:3000/posts/",this.post)
+            .then((result)=>{
+                console.warn("result",result);
+            });
+            //console.warn(this.post);
+            e.preventDefault();
+        }
+    }
+}
+</script>
