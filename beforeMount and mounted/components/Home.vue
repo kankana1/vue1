17c5122cd2a0a480550b3aa6860dbde89663916c
Kankana Kashyap <kashyap.kankana@gmail.com>
2021-08-02 10:33:38 +0530

    Add files via upload

---
+++ beforeMount and mounted/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1 id="demo">{{name}}</h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'Home',
+    data()
+    {
+        return{
+            name:"kankana"
+        }
+    },
+    beforeMount()
+    {
+        console.warn("beforeMount");
+        //console.warn(this.$el);
+        console.warn(document.getElementById("demo"));
+    },
+    mounted()
+    {
+        console.warn("mounted");
+        //console.warn(this.$el);
+        console.warn(document.getElementById("demo"));
+        
+    }
+}
+</script>
