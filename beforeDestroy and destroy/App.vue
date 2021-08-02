3561699f77b5cea3ec849f3cf8fe0f43532b2460
Kankana Kashyap <kashyap.kankana@gmail.com>
2021-08-02 10:51:45 +0530

    Add files via upload

---
+++ beforeDestroy and destroy/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>beforeDestroy and destroyed in vue js</h1>
+    <div v-if="display">
+      <Home />
+    </div>
+    <button v-on:click="toggle">Toggle</button>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  data()
+  {
+    return{display:true}
+  },
+  methods:{
+    toggle()
+    {
+      this.display=!this.display;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ beforeDestroy and destroy/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'Home',
+    data()
+    {
+        return{
+            name:"Home Component"
+        }
+    },
+    beforeDestroy()
+    {
+        alert("before");
+        console.warn("Before Destroy");
+    },
+    destroyed()
+    {
+        alert("destroyed");
+        console.warn("destroyed");
+        
+    }
+}
+</script>
